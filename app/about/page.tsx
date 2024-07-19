b6df463e56b184877c0b9c806fb21128a35efe19
Daniel Krygsman <dkrygsma@ucsc.edu>
2024-07-19 14:13:26 -0700

    adding about

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+export default async function AboutPage() {
+    return <div className="container max-w-6xl py-6 lg:py-10">
+        <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
+            <div className="flex-1 space-x-4">
+                <h1 className="inline-block font-black text-4xl lg:text-5xl">
+                    About me 
+                </h1>
+            </div>
+        </div>
+    </div>
+}
