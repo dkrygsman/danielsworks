32482aeae662556927bda770ba06856ef8e895f1
Daniel Krygsman <dkrygsma@ucsc.edu>
2024-07-25 14:37:30 -0700

    adding template comment

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -34,9 +34,9 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
             <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
                 <div className="flex-1 space-y-4">
                     <h1 className="inline-block font-black text-4xl  lg:text-5xl">Projects</h1>
-                {/* <p className="text-xl text-muted-foreground">
-                    This is text for what i want.
-                </p> */}
+                <p className="text-xl text-muted-foreground">
+                    Template projects for now
+                </p>
                 </div>
             </div>
             <hr className="mt-8" />
