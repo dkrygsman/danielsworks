af04066bbb78f141b8b130a7fe41d11b318eaa84
Daniel Krygsman <dkrygsma@ucsc.edu>
2024-09-27 14:28:27 -0700

    jpeg

--- components/MyResumePic.tsx
+++ components/MyResumePic.tsx
@@ -5,7 +5,7 @@ export default function MyResumePic() {
         <section className="w-full mx-auto">
             <Image 
                 className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
-                src="/images/resume_9_26_2024_JPEG.jpeg"
+                src="/images/resume_9_26_2024_JPEG.jpg"
                 width={200}
                 height={200}
                 alt="resumepic"
