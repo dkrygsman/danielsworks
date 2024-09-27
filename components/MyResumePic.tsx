ba9c7be2fb2cb62dd7ad2cdd6fc35c748500d75e
Daniel Krygsman <dkrygsma@ucsc.edu>
2024-09-27 14:30:10 -0700

    format

--- components/MyResumePic.tsx
+++ components/MyResumePic.tsx
@@ -4,10 +4,10 @@ export default function MyResumePic() {
     return (
         <section className="w-full mx-auto">
             <Image 
-                className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
+                className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black mx-auto mt-8"
                 src="/images/resume_9_26_2024_JPEG.jpg"
-                width={200}
-                height={200}
+                width={400}
+                height={400}
                 alt="resumepic"
                 priority={true}
             />
