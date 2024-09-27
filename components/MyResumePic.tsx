cb1174d09caebdd693ad78a7a413108e512d0bab
Daniel Krygsman <dkrygsma@ucsc.edu>
2024-09-27 14:48:29 -0700

    adding boreder

--- components/MyResumePic.tsx
+++ components/MyResumePic.tsx
@@ -4,7 +4,7 @@ export default function MyResumePic() {
     return (
         <section className="w-full mx-auto">
             <Image 
-                className=" drop-shadow-xl shadow-black mx-auto mt-8"
+                className="border border-slate-700 drop-shadow-xl shadow-black mx-auto mt-8"
                 src="/images/resume_9_26_2024_JPEG.jpg"
                 width={600}
                 height={600}
