13f83a6a9324e96e0d4ac1a5b2b78e0d8388a927
Daniel Krygsman <dkrygsma@ucsc.edu>
2024-11-06 12:07:08 -0800

    rows

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -31,7 +31,7 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
 
     return (
         <div className="container max-w-4xl py-6 lg:py-10">
-            <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
+            <div className="flex flex-row items-start gap-4 md:flex-row md:justify-between md:gap-8">
                 <div className="flex-1 space-y-4">
                     <h1 className="inline-block font-black text-4xl  lg:text-5xl">Projects</h1>
                 {/* <p className="text-xl text-muted-foreground">
