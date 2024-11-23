dad232f8a6802202bd296536cacd5be2aee55fed
Daniel Krygsman <dkrygsma@ucsc.edu>
2024-11-23 11:26:45 -0800

    using styles.css for background

--- components/post-item.tsx
+++ components/post-item.tsx
@@ -12,7 +12,7 @@ interface PostItemProps{
 }
 
 export function PostItem({slug, title, description,date}: PostItemProps) {
-    return <article className="bg-[url('/img/bot_pics.png')] flex flex-col gap-2 border-border border-b py-3">
+    return <article className="flex flex-col gap-2 border-border border-b py-3">
         <div>
             <h2 className="text-2xl font-bold">
                 <Link href={slug}>{title}</Link>
